5d9d0f304d1fd4638a87320facbd472e6ef0b8e7
Timon Emken <emken@chalmers.se>
2020-03-25 15:47:33 +0100

    Add CMake.

---
+++ tests/tests.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <chrono>
+#include <cmath>
+
+#include "Linear_Algebra.hpp"
+#include "Natural_Units.hpp"
+#include "Numerics.hpp"
+#include "Statistics.hpp"
+#include "Utilities.hpp"
+
+int main()
+{
+	//Starting time
+	std::chrono::high_resolution_clock::time_point time_start = std::chrono::high_resolution_clock::now();
+	
+	//1. Tests of functions in Linear_Algebra.hpp"
+	Vector v({1,3,5});
+	std::cout<<v<<std::endl;
+	
+	//Ending time and computing time
+	std::chrono::high_resolution_clock::time_point time_end = std::chrono::high_resolution_clock::now();
+	double durationTotal =1e-6*std::chrono::duration_cast<std::chrono::microseconds>( time_end - time_start ).count();
+	std::cout <<"\nProcessing Time:\t"<< durationTotal<<"s ("<< floor(durationTotal/3600.0)<<":"<<floor(fmod(durationTotal/60.0,60.0))<<":"<<floor(fmod(durationTotal,60.0))<<":"<<floor(fmod(1000*durationTotal,1000.0))<<")."<<std::endl
+	<<"##############################"<<std::endl;
+	
+	return 0;
+}
